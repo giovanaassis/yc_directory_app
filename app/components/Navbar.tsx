ffeaddac0a066ed3e83f2483b09913e5a332365f
giovanaassis <giovana_2011_11@hotmail.com>
2025-06-09 17:57:09 -0300

    feat: added auth setup

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.png";
+import { auth, signIn, signOut } from "@/auth";
 
-function Navbar() {
+export default async function Navbar() {
+  const session = await auth();
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-white p-4 shadow-sm flex items-center justify-between">
+      <Link href="/">
+        <Image src={Logo} alt="Logo Image" width={200} height={30} />
+      </Link>
+      <div className="flex items-center gap-5 text-black">
+        {session && session?.user ? (
+          <>
+            <Link href="/startup/create">
+              <span>Create</span>
+            </Link>
 
-export default Navbar
+            <form
+              action={async () => {
+                "use server";
+
+                await signOut({ redirectTo: "/" });
+              }}
+            >
+              <button type="submit">Logout</button>
+            </form>
+
+            <Link href={`/user/${session?.user?.id}`}>
+              <span>{session?.user?.name}</span>
+            </Link>
+          </>
+        ) : (
+          <form
+            action={async () => {
+              "use server";
+
+              await signIn("github");
+            }}
+          >
+            <button type="submit">Login</button>
+          </form>
+        )}
+      </div>
+    </nav>
+  );
+}
